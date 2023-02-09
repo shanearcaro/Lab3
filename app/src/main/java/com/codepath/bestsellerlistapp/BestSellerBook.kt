d0df20f9c092ec0f4b531c4db0c04c3722531238
Shane Arcaro <smarcaro@gmail.com>
2023-02-09 16:07:33 -0500

    Adding files

---
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBook.kt
@@ -0,0 +1,38 @@
+package com.codepath.bestsellerlistapp
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class BestSellerBook {
+    @SerializedName("rank")
+    var rank = 0
+
+    @JvmField
+    @SerializedName("title")
+    var title: String? = null
+
+    @JvmField
+    @SerializedName("author")
+    var author: String? = null
+
+    //TODO bookImageUrl
+    @JvmField
+    @SerializedName("book_image")
+    var bookImage: String? = null
+
+
+    //TODO description
+    @JvmField
+    @SerializedName("description")
+    var description: String? = null
+
+    //TODO-STRETCH-GOALS amazonUrl
+    @JvmField
+    @SerializedName("amazon_product_url")
+    var amazonURL: String? = null
+}
